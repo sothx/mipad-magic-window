0c657eb91e9158978751dffde951bff91241c683
sothx <i@sothx.com>
2024-02-03 23:01:24 +0800

    feat: 更新build任务

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,6 @@ exports.build = buildTasks
 
 exports.release = buildRelease
 
-const publishTasks = series(build, buildRelease)
+const publishTasks = series(buildTasks, buildRelease)
 
 exports.publish = publishTasks
