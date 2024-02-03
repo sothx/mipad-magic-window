8d33d43665010adbe889265bfdc16f780808aadb
sothx <i@sothx.com>
2024-02-04 01:49:44 +0800

    feat: 删除冗余的tasks

--- tasks/createEnv.js
+++
@@ -1,9 +0,0 @@
-const gulpzip = require('gulp-zip');
-const { src, dest } = require('gulp');
-const moduleConfig = require('../module_src/module.config.json')
-
-module.exports = function buildRelease() {
-  return src('dist/**')
-    .pipe(gulpzip(`${moduleConfig.version}.zip`))
-    .pipe(dest('release'))
-}
