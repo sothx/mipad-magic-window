d476d4eb20abe7fb0d2ce1e4768cec0a44c6eb60
sothx <i@sothx.com>
2024-02-04 23:05:24 +0800

    feat: 补充环境参数

---
+++ config/process.env.js
@@ -0,0 +1,15 @@
+const minimist = require("minimist");
+
+// 配置命令行可以接受的参数以及默认值
+let knownOptions = {
+  string: ['use_platform'],
+  default: {
+    use_platform: "pad" // 平板则为pad，折叠屏则为fold
+  },
+};
+
+const options = minimist(process.argv.slice(2), knownOptions);
+
+module.exports = {
+  options
+}
--- tasks/buildEjsTemplate.js
+++ tasks/buildEjsTemplate.js
@@ -1,11 +1,13 @@
 const gulpEjs = require('gulp-ejs');
 const gulpRename = require('gulp-rename');
 const { src, dest } = require('gulp');
+const { options } = require('../config/process.env');
 
 module.exports = function buildEjsTemplate() {
+  const use_platform = options.use_platform
   return src('module_src/*.ejs')
     .pipe(gulpEjs({
-      age: 13
+      platform: use_platform
     }))
     .pipe(gulpRename({ extname: '.xml' }))
     .pipe(dest('temp'))
