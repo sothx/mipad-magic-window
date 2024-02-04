da8c638f86d2d5a4c3099bf681746e2e19c78474
sothx <i@sothx.com>
2024-02-05 00:29:25 +0800

    feat: 补充相关脚本命令

--- tasks/buildRelease.js
+++ tasks/buildRelease.js
@@ -1,9 +1,11 @@
 const gulpzip = require('gulp-zip');
 const { src, dest } = require('gulp');
-const moduleConfig = require('../config/module.config.json')
+const moduleConfig = require('../config/module.config.json');
+const { options } = require('../config/process.env');
+
 
 module.exports = function buildRelease() {
   return src('dist/**')
     .pipe(gulpzip(`${moduleConfig.version}.zip`))
-    .pipe(dest('release'))
+    .pipe(dest(`release/${options.use_platform}/`))
 }
