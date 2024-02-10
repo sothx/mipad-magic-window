9b271b61e6402d65fe0f6f25e6e31bbbfaa30cde
i <i@sothx.com>
2024-02-10 09:44:37 +0000

    feat: 完善新属性补充

--- config/process.env.js
+++ config/process.env.js
@@ -3,11 +3,14 @@ const minimist = require("minimist");
 // 配置命令行可以接受的参数以及默认值
 let knownOptions = {
   string: ['use_platform'],
+  boolean: ['use_ext'],
   default: {
-    use_platform: "pad" // 平板则为pad，折叠屏则为fold
+    use_platform: "pad", // 平板则为pad，折叠屏则为fold
+    use_ext: false,// true为混入扩展配置，false则不混入扩展配置
   },
   alias: {
-    p: 'use_platform'
+    p: 'use_platform',
+    e: 'use_ext'
   }
 };
 
--- tasks/buildExtConfig.js
+++ tasks/buildExtConfig.js
@@ -11,12 +11,9 @@ const fs = require('fs')
 let extConfigStack = {}
 
 
-const buildActionIsFold = function() {
-  const use_platform = options.use_platform
-  if (use_platform === 'fold') {
-    return true;
-  }
-  return false;
+const isNeedExtConfig = function() {
+  const use_ext = options.use_ext
+  return use_ext;
 }
 
 /**
@@ -24,23 +21,23 @@ const buildActionIsFold = function() {
  */
 function getExtConfigData() {
   return src('temp/ext/embedded_rules_list.xml') // 指定XML文件的路径
-    .pipe(gulpXML({
-        callback: function(result) {
-          const doc = new DOMParser().parseFromString(result, 'text/xml');
-          const elementsWithAttribute = doc.getElementsByTagName('package');
-          // console.log(doc,'doc')
-          for (let i = 0; i < elementsWithAttribute.length; i++) {
-            const attrs = elementsWithAttribute[i].attributes;
-            const currentAttrName = elementsWithAttribute[i].getAttribute('name')
-            if (currentAttrName) {
-              extConfigStack[currentAttrName] = {}
-              for (var j = attrs.length - 1; j >= 0; j--) {
-                extConfigStack[currentAttrName][attrs[j].name] = attrs[j].value
-              }
+    .pipe(gulpIf(isNeedExtConfig,gulpXML({
+      callback: function(result) {
+        const doc = new DOMParser().parseFromString(result, 'text/xml');
+        const elementsWithAttribute = doc.getElementsByTagName('package');
+        // console.log(doc,'doc')
+        for (let i = 0; i < elementsWithAttribute.length; i++) {
+          const attrs = elementsWithAttribute[i].attributes;
+          const currentAttrName = elementsWithAttribute[i].getAttribute('name')
+          if (currentAttrName) {
+            extConfigStack[currentAttrName] = {}
+            for (var j = attrs.length - 1; j >= 0; j--) {
+              extConfigStack[currentAttrName][attrs[j].name] = attrs[j].value
             }
           }
         }
-      }))
+      }
+    })))
     .pipe(gulpRename({
       extname: '.json'
     }))
@@ -49,7 +46,7 @@ function getExtConfigData() {
 
 function mergeExtConfig() {
   return src('temp/embedded_rules_list.xml') // 指定XML文件的路径
-    .pipe(gulpXML({
+    .pipe(gulpIf(isNeedExtConfig,gulpXML({
       callback: function (result) {
         const doc = new DOMParser().parseFromString(result, 'text/xml')
         // 获取根节点  
@@ -66,10 +63,12 @@ function mergeExtConfig() {
   
           // 创建一个新元素  
           const newElement = doc.createElement('package');
-            
+          newElement.setAttribute('name',key)
           for (const [vKey,vValue] of Object.entries(value)) {
-            // 为新元素设置属性
-            newElement.setAttribute(vKey, vValue);
+            if (vKey !== 'name') {
+              // 为新元素设置属性
+              newElement.setAttribute(vKey, vValue);
+            }
           }
           // 创建一个包含两个空格的文本节点  
           const spaceTextNode = doc.createTextNode('  '); // 两个空格  
@@ -84,7 +83,7 @@ function mergeExtConfig() {
         // 使用正则表达式删除空行  
         const cleanedXml = serializedXml.replace(/^\s*[\r\n]|[\r\n]+\s*$/gm, ''); 
         return cleanedXml;
-      }}))
+      }})))
     .pipe(dest('temp'));
 }
 
