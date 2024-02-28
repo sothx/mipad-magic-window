c1bfd6232234c168a351d6e7525289b3249d6fe1
sothx <i@sothx.com>
2024-02-28 16:22:05 +0800

    feat: 拆离安卓11的配置

--- tasks/buildTemplate.js
+++ tasks/buildTemplate.js
@@ -1,6 +1,15 @@
 const { src, dest } = require('gulp');
+const { options } = require('../config/process.env');
+
+const buildActionUseModeIsMagicWindow = function () {
+  return options.use_mode === 'magicWindow'
+}
+
+const buildActionUseModeIsActivityEmbedding = function () {
+  return options.use_mode === 'activityEmbedding'
+}
 
 module.exports = function buildTemplate() {
-  return src('install_module_template/**')
-  .pipe(dest('dist'))
+  return src(buildActionUseModeIsMagicWindow ? 'install_module_template/magicWindow/**' : (buildActionUseModeIsActivityEmbedding ? 'install_module_template/activityEmbedding/**' : ''))
+    .pipe(dest('dist'))
 }
