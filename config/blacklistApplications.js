a5afdc2309fe80837f0bc313ff5c12db5fa892d5
sothx <i@sothx.com>
2024-03-03 03:13:06 +0800

    feat: 更新

--- config/blacklistApplications.js
+++ config/blacklistApplications.js
@@ -3,5 +3,7 @@ module.exports = {
   'com.tencent.wework': true, // 企业微信
   'com.taobao.trip': true, // 飞猪
   'com.fenbi.android.leo': true, // 小猿口算
-  'com.duokan.reader': true// 多看阅读
+  'com.duokan.reader': true,// 多看阅读
+  'com.xiaomi.shop': true, // 小米商城
+  'com.xiaomi.smarthome': true, // 米家
 }
