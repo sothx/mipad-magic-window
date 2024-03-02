fe9f3572ab71f9d472486db4dca914da9434b7b9
sothx <i@sothx.com>
2024-03-03 02:52:16 +0800

    feat: 移除多看阅读的平行视界适配

--- config/blacklistApplications.js
+++ config/blacklistApplications.js
@@ -2,5 +2,6 @@ module.exports = {
   'com.taobao.taobao': true, // 淘宝
   'com.tencent.wework': true, // 企业微信
   'com.taobao.trip': true, // 飞猪
-  'com.fenbi.android.leo': true // 小猿口算
+  'com.fenbi.android.leo': true, // 小猿口算
+  'com.duokan.reader': true// 多看阅读
 }
