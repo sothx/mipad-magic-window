218338a2ad0ecebd59bf851239d90e06b9979148
sothx <i@sothx.com>
2024-03-04 02:25:10 +0800

    feat: 调整网盘目录结构

--- tasks/buildNetdiskRelease.js
+++ tasks/buildNetdiskRelease.js
@@ -16,29 +16,29 @@ const releaseNetdiskDir = `release/${moduleConfig.version}/netdisk`
 
 function releasePad() {
   return src(`${releaseDir}/${moduleConfig.version}/pad-${moduleConfig.version}.zip`)
-    .pipe(dest(`${releaseNetdiskDir}/1.小米平板专版（推荐）`))
+    .pipe(dest(`${releaseNetdiskDir}/1.小米平板专版（推荐）/模块`))
 }
 
 function releaseRatioOf3To2Pad() {
   return src(`${releaseDir}/${moduleConfig.version}/pad-ratioOf3To2-${moduleConfig.version}.zip`)
-    .pipe(dest(`${releaseNetdiskDir}/2.小米平板6S Pro专版(适配3比2比例)`))
+    .pipe(dest(`${releaseNetdiskDir}/2.小米平板6S Pro专版(适配3比2比例)/模块`))
 }
 
 function releaseFold() {
   return src(`${releaseDir}/${moduleConfig.version}/fold-${moduleConfig.version}.zip`)
-    .pipe(dest(`${releaseNetdiskDir}/3.小米折叠屏专版（适配Mix Fold系列）`))
+    .pipe(dest(`${releaseNetdiskDir}/3.小米折叠屏专版（适配Mix Fold系列）/模块`))
 }
 
 
 function releasePadByMagicWindow() {
   return src(`${releaseDir}/${moduleConfig.version}/pad-magicWindow-${moduleConfig.version}.zip`)
-    .pipe(dest(`${releaseNetdiskDir}/4.小米平板老魔窗专版（适配安卓11）`))
+    .pipe(dest(`${releaseNetdiskDir}/4.小米平板老魔窗专版（适配安卓11）/模块`))
 }
 
 
 function releasePadByMIUI14Transplant() {
   return src(`${releaseDir}/${moduleConfig.version}/transplant-${moduleConfig.version}.zip`)
-    .pipe(dest(`${releaseNetdiskDir}/5.基于MIUI14的6 Max移植包专版（仅MIUI14移植可刷）`))
+    .pipe(dest(`${releaseNetdiskDir}/5.基于MIUI14的6 Max移植包专版（仅MIUI14移植可刷）/模块`))
 }
 
 
