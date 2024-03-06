d17e70d5d9c047dac92aef2333aa8bee6cf65dce
sothx <i@sothx.com>
2024-03-07 00:35:01 +0800

    feat: 完善版本

--- tasks/buildRelease.js
+++ tasks/buildRelease.js
@@ -3,9 +3,8 @@ const { src, dest } = require('gulp');
 const moduleConfig = require('../config/module.config.json');
 const { options } = require('../config/process.env');
 
-
 module.exports = function buildRelease() {
   return src('dist/**')
-    .pipe(gulpzip(`${options.is_transplant ? 'transplant' : (options.use_compatibility === 'supported_show_divider' ? 'supportedShowDivider' : options.use_platform)}${options.use_ext ? `-ext` : ''}${options.use_mode === 'magicWindow' ? '-magicWindow' : ''}${options.use_ratio === '3:2' ? '-ratioOf3To2' : ''}-${moduleConfig.version}.zip`))
+    .pipe(gulpzip(`${options.is_transplant ? 'transplant' : options.use_platform}${options.use_ext ? `-ext` : ''}${options.use_mode === 'magicWindow' ? '-magicWindow' : ''}${options.use_ratio === '3:2' ? '-ratioOf3To2' : ''}${options.use_compatibility ? `${'-' + options.use_compatibility}` : ''}-${moduleConfig.version}.zip`))
     .pipe(dest(`release/${moduleConfig.version}${options.use_ext ? '/ext' : ''}`))
 }
