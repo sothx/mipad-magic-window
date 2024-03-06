ce7505dc736a95fba487ddd87d82dbca3699820c
sothx <i@sothx.com>
2024-03-07 00:26:09 +0800

    feat: 调整兼容性适配

--- tasks/buildRelease.js
+++ tasks/buildRelease.js
@@ -6,6 +6,6 @@ const { options } = require('../config/process.env');
 
 module.exports = function buildRelease() {
   return src('dist/**')
-    .pipe(gulpzip(`${options.is_transplant ? 'transplant' : options.use_platform}${options.use_ext ? `-ext` : ''}${options.use_mode === 'magicWindow' ? '-magicWindow' : ''}${options.use_ratio === '3:2' ? '-ratioOf3To2' : ''}-${moduleConfig.version}.zip`))
+    .pipe(gulpzip(`${options.is_transplant ? 'transplant' : (options.use_compatibility ? options.use_compatibility : options.use_platform)}${options.use_ext ? `-ext` : ''}${options.use_mode === 'magicWindow' ? '-magicWindow' : ''}${options.use_ratio === '3:2' ? '-ratioOf3To2' : ''}-${moduleConfig.version}.zip`))
     .pipe(dest(`release/${moduleConfig.version}${options.use_ext ? '/ext' : ''}`))
 }
