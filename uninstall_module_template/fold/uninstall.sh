9ae762edf458d29ac16c14fbd5fbf5ec44c27d01
sothx <i@sothx.com>
2024-03-09 00:00:21 +0800

    feat: 完善模块配置

--- uninstall_module_template/fold/uninstall.sh
+++ uninstall_module_template/fold/uninstall.sh
@@ -2,8 +2,8 @@
 MODDIR=${0%/*}
 
 # Remove Files
-rm /data/system/users/0/magic_window_setting_config.xml
-rm /data/system/magicWindowFeature_magic_window_application_list.xml
+# rm /data/system/users/0/magic_window_setting_config.xml
+# rm /data/system/magicWindowFeature_magic_window_application_list.xml
 
 # Move Back Files
 rm /product/etc/embedded_rules_list.xml
@@ -11,6 +11,11 @@ cp $MODDIR/common/product/etc/embedded_rules_list_bak /product/etc/embedded_rule
 rm /data/system/cloudFeature_embedded_rules_list.xml
 cp $MODDIR/common/product/etc/embedded_rules_list_bak /data/system/cloudFeature_embedded_rules_list.xml
 rm /data/system/users/0/embedded_setting_config.xml
+
+rm /product/etc/fixed_orientation_list.xml
+cp $MODDIR/common/product/etc/fixed_orientation_list_bak /product/etc/fixed_orientation_list.xml
+rm /data/system/cloudFeature_fixed_orientation_list.xml
+cp $MODDIR/common/product/etc/fixed_orientation_list_bak /data/system/cloudFeature_fixed_orientation_list.xml
 
 # Enable Cloud Feature
 # chmod 660 /product/etc/embedded_rules_list.xml
