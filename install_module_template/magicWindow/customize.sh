8f47ee6bd1eb7cc826d513acbdcf795d6ab3ce9e
sothx <i@sothx.com>
2024-03-11 19:39:05 +0800

    feat: 完善模块写保护

--- install_module_template/magicWindow/customize.sh
+++ install_module_template/magicWindow/customize.sh
@@ -2,5 +2,7 @@
 SKIPUNZIP=0
 
 # 移除模块文件写保护
+ui_print "- 正在移除模块文件的写保护"
 chattr -R -i /data/system/cloudFeature_embedded_rules_list.xml
 chattr -R -i /data/system/cloudFeature_fixed_orientation_list.xml
+ui_print "- 已经移除模块文件写保护"
