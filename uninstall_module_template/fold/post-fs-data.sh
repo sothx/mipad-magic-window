1c66653bf7e413e525e24a1de02d92c59a0a6172
sothx <i@sothx.com>
2024-03-12 17:59:07 +0800

    feat: 完善卸载模块

--- uninstall_module_template/fold/post-fs-data.sh
+++ uninstall_module_template/fold/post-fs-data.sh
@@ -8,6 +8,9 @@ MODDIR=${0%/*}
 chattr -R -i /data/adb/modules/MIUI_MagicWindow+
 
 # For Android 11
+# 兼容部分移植包的修改
+chattr -i /data/system/users/0/magic_window_setting_config.xml
+chattr -i /data/system/magicWindowFeature_magic_window_application_list.xml
 # 直接删除A11配置文件，重启后系统会自动重新生成
 rm /data/system/users/0/magic_window_setting_config.xml
 rm /data/system/magicWindowFeature_magic_window_application_list.xml
