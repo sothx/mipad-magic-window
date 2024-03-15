c69872b7fa7ff3cdd10fc8ba6c71fd5e344bd5be
sothx <i@sothx.com>
2024-03-16 02:43:11 +0800

    feat: 完善版本日志

--- tasks/buildRelease.js
+++ tasks/buildRelease.js
@@ -3,7 +3,7 @@ const { src, dest } = require('gulp');
 const moduleConfig = require('../config/module.config.json');
 const { options } = require('../config/process.env');
 
-module.exports = function buildRelease() {
+module.exports = function buildRelease(cb) {
   return src('dist/**')
     .pipe(gulpzip(`${options.is_uninstall_package  ? 'uninstall-' : ''}${options.is_transplant ? 'transplant' : options.use_platform}${options.use_ext ? `-ext` : ''}${options.use_mode === 'magicWindow' ? '-magicWindow' : ''}${options.use_ratio === '3:2' ? '-ratioOf3To2' : ''}${options.use_compatibility ? `${'-' + options.use_compatibility}` : ''}-${options.is_uninstall_package ? '0.00.00' : moduleConfig.version}.zip`))
     .pipe(dest(`release/${moduleConfig.version}${options.use_ext ? '/ext' : ''}`))
--- tasks/buildUpdateMsg.js
+++ tasks/buildUpdateMsg.js
@@ -2,18 +2,23 @@ const gulpzip = require('gulp-zip');
 const { src, dest } = require('gulp');
 const moduleConfig = require('../config/module.config.json');
 const { options } = require('../config/process.env');
-const gulpFile = require('gulp-file');
+const gulpJSONEdit = require('gulp-json-editor');
+const gulpRename = require('gulp-rename');
 
 const packageName = `${options.is_transplant ? 'transplant' : options.use_platform}${options.use_mode === 'magicWindow' ? '-magicWindow' : ''}${options.use_ratio === '3:2' ? '-ratioOf3To2' : ''}${options.use_compatibility ? `${'-' + options.use_compatibility}` : ''}`
 
-module.exports = function buildUpdateMsg() {
-  return src('')
-    .pipe(gulpFile(`${packageName}.json`, JSON.stringify({
-      version: moduleConfig.version,
-      versionCode: Number(moduleConfig.versionCode),
-      zipUrl: `https://github.com/sothx/mipad-magic-window/releases/download/${moduleConfig.version}/${packageName}-${moduleConfig.version}.zip`,
-      changelog: 'https://hyper-magic-window-module-update.sothx.com/changelog.md'
-    })))
+module.exports = function buildUpdateMsg(cb) {
+  const unNeedUpdate = !options.is_uninstall_package && !options.use_ext
+  return unNeedUpdate ? cb() : src('module_src/template/release.json')
+    .pipe(gulpJSONEdit(function (json) {
+      json.version = moduleConfig.version
+      json.versionCode = Number(moduleConfig.versionCode)
+      json.zipUrl = `https://github.com/sothx/mipad-magic-window/releases/download/${moduleConfig.version}/${packageName}-${moduleConfig.version}.zip`
+      json.changelog = 'https://hyper-magic-window-module-update.sothx.com/changelog.md'
+      return json;
+    }))
+    .pipe(gulpRename(`${packageName}.json`))
+    .pipe(dest(`docs/release/`))
   // return src('dist/**')
   //   .pipe(gulpzip(`${options.is_uninstall_package  ? 'uninstall-' : ''}${options.is_transplant ? 'transplant' : options.use_platform}${options.use_ext ? `-ext` : ''}${options.use_mode === 'magicWindow' ? '-magicWindow' : ''}${options.use_ratio === '3:2' ? '-ratioOf3To2' : ''}${options.use_compatibility ? `${'-' + options.use_compatibility}` : ''}-${options.is_uninstall_package ? '0.00.00' : moduleConfig.version}.zip`))
   //   .pipe(dest(`release/${moduleConfig.version}${options.use_ext ? '/ext' : ''}`))
