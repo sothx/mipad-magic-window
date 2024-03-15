cc58fc307f4d7f976be6d4b6e265ca1d557a0872
sothx <i@sothx.com>
2024-03-15 14:49:10 +0800

    feat: 优化兼容性

--- install_module_template/magicWindow/customize.sh
+++ install_module_template/magicWindow/customize.sh
@@ -3,7 +3,9 @@ SKIPUNZIP=0
 
 # 移除模块文件写保护
 ui_print "- 正在移除《HyperOS For Pad/Fold 完美横屏应用计划》模块文件的写保护……"
-chattr -R -i /data/system/users/0/magic_window_setting_config.xml
-chattr -R -i /data/system/magicWindowFeature_magic_window_application_list.xml
+# 对云控文件解除写保护
+chattr -R -i /data/adb/modules/MIUI_MagicWindow+
+chattr -i /data/system/users/0/magic_window_setting_config.xml
+chattr -i /data/system/magicWindowFeature_magic_window_application_list.xml
 ui_print "- 已经移除《HyperOS For Pad/Fold 完美横屏应用计划》模块文件的写保护！"
 ui_print "- 《HyperOS For Pad/Fold 完美横屏应用计划》安装/更新完成，重启系统后生效！"
