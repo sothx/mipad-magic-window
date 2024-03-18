b8d1234b34075b703bd1a966186916299d4debdc
sothx <i@sothx.com>
2024-03-19 05:41:33 +0800

    feat: 完善测试版

--- tasks/buildNetdiskRelease.js
+++ tasks/buildNetdiskRelease.js
@@ -19,35 +19,35 @@ function isSothxExtBuild() {
 
 function releasePad() {
   return src(`${releaseDir}/${moduleConfig.version}/pad-${moduleConfig.version}.zip`)
-    .pipe(dest(`${releaseNetdiskDir}/1.小米平板通用版（通用版，推荐）/模块`))
+    .pipe(dest(`${releaseNetdiskDir}/1.小米平板通用版（通用版，推荐）/模块(测试版)`))
 }
 
 function releaseRatioOf3To2Pad() {
   return src(`${releaseDir}/${moduleConfig.version}/pad-ratioOf3To2-${moduleConfig.version}.zip`)
-    .pipe(dest(`${releaseNetdiskDir}/2.小米平板6S Pro专版(适配3比2比例)/模块`))
+    .pipe(dest(`${releaseNetdiskDir}/2.小米平板6S Pro专版(适配3比2比例)/模块(测试版)`))
 }
 
 function releaseFold() {
   return src(`${releaseDir}/${moduleConfig.version}/fold-${moduleConfig.version}.zip`)
-    .pipe(dest(`${releaseNetdiskDir}/3.小米折叠屏通用版（适配Mix Fold系列）/模块`))
+    .pipe(dest(`${releaseNetdiskDir}/3.小米折叠屏通用版（适配Mix Fold系列）/模块(测试版)`))
 }
 
 
 function releasePadByMagicWindow() {
   return src(`${releaseDir}/${moduleConfig.version}/pad-magicWindow-${moduleConfig.version}.zip`)
-    .pipe(dest(`${releaseNetdiskDir}/4.小米平板安卓11专版/模块`))
+    .pipe(dest(`${releaseNetdiskDir}/4.小米平板安卓11专版/模块(测试版)`))
 }
 
 function releaseHyperOsForPad5() {
   return src(`${releaseDir}/${moduleConfig.version}/pad-not-dragable-${moduleConfig.version}.zip`)
-    .pipe(dest(`${releaseNetdiskDir}/5.小米平板安卓13专版（无滑动调节）/模块`))
+    .pipe(dest(`${releaseNetdiskDir}/5.小米平板安卓13专版（无滑动调节）/模块(测试版)`))
 }
 
 
 
 function releasePadByMIUI14Transplant() {
   return src(`${releaseDir}/${moduleConfig.version}/transplant-${moduleConfig.version}.zip`)
-    .pipe(dest(`${releaseNetdiskDir}/6.基于MIUI14的6 Max移植包专版（仅MIUI14移植可刷）/模块`))
+    .pipe(dest(`${releaseNetdiskDir}/6.基于MIUI14的6 Max移植包专版（仅MIUI14移植可刷）/模块(测试版)`))
 }
 
 function releaseUnInstallPad() {
@@ -67,7 +67,7 @@ function releaseUnInstallMagicWindow() {
 
 function releaseExt(cb) {
   return isSothxExtBuild() ? src(`${releaseDir}/${moduleConfig.version}/ext/pad-ext-${moduleConfig.version}.zip`)
-    .pipe(dest(`${releaseNetdiskDir}/X.自用版(基于通用版，仅根据我的习惯适配)/模块`)) : cb()
+    .pipe(dest(`${releaseNetdiskDir}/X.自用版(基于通用版，仅根据我的习惯适配)/模块(测试版)`)) : cb()
 }
 
 
