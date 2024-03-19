86af14361d992c3203512af508517a7d6c3cf817
sothx <i@sothx.com>
2024-03-19 18:09:36 +0800

    feat: 调整配置

--- install_module_template/activityEmbedding/post-fs-data.sh
+++ install_module_template/activityEmbedding/post-fs-data.sh
@@ -2,29 +2,33 @@
 # 请不要硬编码 /magisk/modname/... ; 请使用 $MODDIR/...
 # 这将使你的脚本更加兼容，即使Magisk在未来改变了它的挂载点
 MODDIR=${0%/*}
-# DIY_CONFIG_EMBEDDED_RULES_LIST="/data/adb/MIUI_MagicWindow+/config/embedded_rules_list.xml"
-# DIY_CONFIG_FIXED_ORIENTATION_LIST="/data/adb/MIUI_MagicWindow+/config/fixed_orientation_list.xml"
+DIY_CONFIG_EMBEDDED_RULES_LIST="/data/adb/MIUI_MagicWindow+/config/embedded_rules_list.xml"
+DIY_CONFIG_FIXED_ORIENTATION_LIST="/data/adb/MIUI_MagicWindow+/config/fixed_orientation_list.xml"
 
-# # 对云控文件解除写保护
-# chattr -i /data/system/cloudFeature_embedded_rules_list.xml
-# chattr -i /data/system/cloudFeature_fixed_orientation_list.xml
+# 对云控文件解除写保护
+chattr -i /data/system/cloudFeature_embedded_rules_list.xml
+chattr -i /data/system/cloudFeature_fixed_orientation_list.xml
 
-# # 支持自定义配置
-# if [ -f $DIY_CONFIG_EMBEDDED_RULES_LIST ]; then
-#     cp -f $MODDIR/common/product/etc/source/embedded_rules_list.xml $MODDIR/common/product/etc/embedded_rules_list.xml
-#     embedded_rules_packages=$(awk 'BEGIN{RS="<package_config>|</package_config>"} /<package/{print}' $DIY_CONFIG_EMBEDDED_RULES_LIST)
-#     sed -i "/<\/package_config>/i $embedded_rules_packages" $MODDIR/common/product/etc/embedded_rules_list.xml
-# else 
-#     cp -f $MODDIR/common/product/etc/source/embedded_rules_list.xml $MODDIR/common/product/etc/embedded_rules_list.xml
-# fi
+# 支持自定义配置
+if [ -f $DIY_CONFIG_EMBEDDED_RULES_LIST ]; then
+    cp -f $MODDIR/common/product/etc/source/embedded_rules_list.xml $MODDIR/common/product/etc/embedded_rules_list.xml
+    embedded_rules_packages=$(awk 'BEGIN{RS="<package_config>|</package_config>"} /<package/{print}' $DIY_CONFIG_EMBEDDED_RULES_LIST)
+    sed -i "/<\/package_config>/i $embedded_rules_packages" $MODDIR/common/product/etc/embedded_rules_list.xml
+    sed -i "/<\/package_config>/i $embedded_rules_packages" $MODDIR/product/etc/embedded_rules_list.xml
+else 
+    cp -f $MODDIR/common/product/etc/source/embedded_rules_list.xml $MODDIR/common/product/etc/embedded_rules_list.xml
+    cp -f $MODDIR/common/product/etc/source/embedded_rules_list.xml $MODDIR/product/etc/embedded_rules_list.xml
+fi
 
-# if [ -f $DIY_CONFIG_FIXED_ORIENTATION_LIST ]; then
-#     cp -f $MODDIR/common/product/etc/source/fixed_orientation_list.xml $MODDIR/common/product/etc/fixed_orientation_list.xml
-#     fixed_orientation_list_packages=$(awk 'BEGIN{RS="<package_config>|</package_config>"} /<package/{print}' $DIY_CONFIG_FIXED_ORIENTATION_LIST)
-#     sed -i "/<\/package_config>/i $fixed_orientation_list_packages" $MODDIR/common/product/etc/fixed_orientation_list.xml
-# else 
-#     cp -f $MODDIR/common/product/etc/source/fixed_orientation_list.xml $MODDIR/common/product/etc/fixed_orientation_list.xml
-# fi
+if [ -f $DIY_CONFIG_FIXED_ORIENTATION_LIST ]; then
+    cp -f $MODDIR/common/product/etc/source/fixed_orientation_list.xml $MODDIR/common/product/etc/fixed_orientation_list.xml
+    fixed_orientation_list_packages=$(awk 'BEGIN{RS="<package_config>|</package_config>"} /<package/{print}' $DIY_CONFIG_FIXED_ORIENTATION_LIST)
+    sed -i "/<\/package_config>/i $fixed_orientation_list_packages" $MODDIR/common/product/etc/fixed_orientation_list.xml
+    sed -i "/<\/package_config>/i $fixed_orientation_list_packages" $MODDIR/product/etc/fixed_orientation_list.xml
+else 
+    cp -f $MODDIR/common/product/etc/source/fixed_orientation_list.xml $MODDIR/common/product/etc/fixed_orientation_list.xml
+    cp -f $MODDIR/common/product/etc/source/fixed_orientation_list.xml $MODDIR/product/etc/fixed_orientation_list.xml
+fi
 
 # For Android 12+
 # 设置SELinux安全上下文
