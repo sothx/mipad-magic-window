12738deff4200ae8db3adcbb3e3e0c8855625631
sothx <i@sothx.com>
2024-03-20 13:21:11 +0800

    feat: update

--- install_module_template/magicWindow/post-fs-data.sh
+++ install_module_template/magicWindow/post-fs-data.sh
@@ -3,8 +3,10 @@
 # 这将使你的脚本更加兼容，即使Magisk在未来改变了它的挂载点
 MODDIR=${0%/*}
 # For Android 11
-cp $MODDIR/common/system/users/0/magic_window_setting_config.xml /data/system/users/0/magic_window_setting_config.xml
-cp $MODDIR/common/system/magicWindowFeature_magic_window_application_list.xml /data/system/magicWindowFeature_magic_window_application_list.xml
+rm /data/system/users/0/magic_window_setting_config.xml
+cp -l $MODDIR/common/system/users/0/magic_window_setting_config.xml /data/system/users/0/magic_window_setting_config.xml
+rm /data/system/magicWindowFeature_magic_window_application_list.xml
+cp -l $MODDIR/common/system/magicWindowFeature_magic_window_application_list.xml /data/system/magicWindowFeature_magic_window_application_list.xml
 ## 对云控文件写保护
 chattr +i /data/system/users/0/magic_window_setting_config.xml
 chattr +i /data/system/magicWindowFeature_magic_window_application_list.xml
