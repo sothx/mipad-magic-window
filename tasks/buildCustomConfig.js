69861c200f27e59aff18b804c266faac2fb82938
sothx <i@sothx.com>
2024-04-15 02:59:28 +0800

    feat: remove temp

--- tasks/buildCustomConfig.js
+++
@@ -1,5 +0,0 @@
-module.exports = function buildCustomConfig(cb) {
-  return src('../custom_config_template/**/**')
-    .pipe(gulpzip(`custom_config_template_0.00.00.zip`))
-    .pipe(dest(`release/${moduleConfig.version}`))
-}
