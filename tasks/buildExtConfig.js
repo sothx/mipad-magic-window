5a53cd00f57310a74fcb8372c1fb2ea9c2afaf22
sothx <i@sothx.com>
2024-04-24 05:55:35 +0800

    feat: 支持扩展应用布局优化配置

--- tasks/buildExtConfig.js
+++ tasks/buildExtConfig.js
@@ -156,4 +156,45 @@ function mergeFixedOrientationListExtConfig(cb) {
     .pipe(dest('temp'));
 }
 
-module.exports = series(getEmbeddedRuleListExtConfigData,mergeEmbeddedRuleListExtConfig,getFixedOrientationListExtConfigData,mergeFixedOrientationListExtConfig)
+function mergeAutoUIListExtConfig(cb) {
+  return src('temp/autoui_list.xml') // 指定XML文件的路径
+    .pipe(gulpIf(isNeedExtConfig,gulpXML({
+      callback: function (result) {
+        const doc = new DOMParser().parseFromString(result, 'text/xml')
+        // 获取根节点  
+        const packageConfigNode = doc.getElementsByTagName('packageRules')[0]
+        const elementsWithAttribute = doc.getElementsByTagName('package');
+        for (let i = 0; i < elementsWithAttribute.length; i++) {
+          const currentAttrName = elementsWithAttribute[i].getAttribute('name')
+          if (FixedOrientationListExtConfigStack[currentAttrName]) {
+            elementsWithAttribute[i].parentNode.removeChild(elementsWithAttribute[i])
+          }
+        }
+        for (const [key, value] of Object.entries(FixedOrientationListExtConfigStack)) {
+          // 创建一个新元素  
+          const newElement = doc.createElement('package');
+          newElement.setAttribute('name',key)
+          for (const [vKey,vValue] of Object.entries(value)) {
+            if (vKey !== 'name') {
+              // 为新元素设置属性
+              newElement.setAttribute(vKey, vValue);
+            }
+          }
+            // 创建一个包含两个空格的文本节点  
+            const spaceTextNode = doc.createTextNode('  '); // 两个空格  
+            packageConfigNode.appendChild(spaceTextNode);
+            // 将文本节点附加到新元素  
+            packageConfigNode.appendChild(newElement);
+            // 添加换行符
+            const newLineNode = doc.createTextNode('\n');
+            packageConfigNode.appendChild(newLineNode);     
+        }
+        const serializedXml = new XMLSerializer().serializeToString(doc);
+        // 使用正则表达式删除空行  
+        const cleanedXml = serializedXml.replace(/^\s*[\r\n]|[\r\n]+\s*$/gm, ''); 
+        return cleanedXml;
+      }})))
+    .pipe(dest('temp'));
+}
+
+module.exports = series(getEmbeddedRuleListExtConfigData,mergeEmbeddedRuleListExtConfig,getFixedOrientationListExtConfigData,mergeFixedOrientationListExtConfig,mergeAutoUIListExtConfig)
