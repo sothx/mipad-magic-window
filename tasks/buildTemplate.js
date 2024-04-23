791d7410e2fbe081722758d182b98c41eaac630e
sothx <i@sothx.com>
2024-04-23 08:39:38 +0800

    feat: 调整node层

--- tasks/buildTemplate.js
+++ tasks/buildTemplate.js
@@ -2,14 +2,11 @@ const { src, dest } = require('gulp');
 const { options } = require('../config/process.env');
 
 const installTemplateMap = {
-  generic: 'install_module_template/generic/**',
-  transplant: 'install_module_template/transplant/**'
+  generic: 'install_module_template/generic/**'
 }
 
 const getInstallTemplateType = function () {
-  const is_transplant = options.is_transplant
-  const is_pad = options.use_platform === 'pad'
-  return (is_transplant && is_pad) ?  'transplant' : 'generic'
+  return 'generic'
 }
 
 module.exports = function buildTemplate() {
