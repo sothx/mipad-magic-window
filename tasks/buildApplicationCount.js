a9d02be1bdc5ab639a8cefa7323c7c389fe632fd
sothx <i@sothx.com>
2024-05-08 12:23:18 +0800

    fix: 修复自用版统计数据有误的问题

--- tasks/buildApplicationCount.js
+++ tasks/buildApplicationCount.js
@@ -7,20 +7,6 @@ const gulpIf = require('gulp-if');
 const DOMParser = require('xmldom').DOMParser;
 const XMLSerializer = require('xmldom').XMLSerializer;
 
-// const buildSourceSrc = function () {
-//   const use_mode = options.use_mode
-
-//   if (use_mode === 'activityEmbedding') {
-//     return 'temp/embedded_rules_list.xml'
-//   }
-
-//   if (use_mode === 'magicWindow') {
-//     return 'temp/magicWindowFeature_magic_window_application_list.xml'
-//   }
-
-//   return ''
-// }
-
 /**
  * 统计适配应用总数
  */
