5c250df65cc76a656fe2b40bda58dbd97bfface1
sothx <i@sothx.com>
2024-05-08 22:25:24 +0800

    feat: 优化适配应用总数的统计算法

--- tasks/buildApplicationCount.js
+++ tasks/buildApplicationCount.js
@@ -7,16 +7,24 @@ const gulpIf = require('gulp-if');
 const DOMParser = require('xmldom').DOMParser;
 const XMLSerializer = require('xmldom').XMLSerializer;
 
+let packageStack = {}
+
 /**
  * 统计适配应用总数
  */
 module.exports = function buildApplicationCount(cb) {
-  return src(options.use_mode === 'activityEmbedding' ? 'temp/embedded_rules_list.xml' : 'temp/magicWindowFeature_magic_window_application_list.xml') // 指定XML文件的路径
+  return src(options.use_mode === 'activityEmbedding' ? ['temp/embedded_rules_list.xml','temp/fixed_orientation_list.xml','temp/autoui_list.xml'] : 'temp/magicWindowFeature_magic_window_application_list.xml') // 指定XML文件的路径
     .pipe(gulpXML({
       callback: function (result) {
         const doc = new DOMParser().parseFromString(result, 'text/xml')
         const elementsWithAttribute = doc.getElementsByTagName('package');
-        global.applicationRuleCount = elementsWithAttribute.length || ''
+        for (let i = 0; i < elementsWithAttribute.length; i++) {
+          const currentAttrName = elementsWithAttribute[i].getAttribute('name')
+          if (currentAttrName) {
+            packageStack[currentAttrName] = 1
+          }
+        }
+        global.applicationRuleCount = Object.keys(packageStack).length || ''
       }
     }))
 }
