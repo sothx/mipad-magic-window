02a6125d287fc28325cd5eddb3db0a80f398635f
sothx <i@sothx.com>
2024-09-21 03:53:13 +0800

    feat: update

--- module_src/valid_android_target/LEToUpsideDownCake/valid_target.sh
+++ module_src/valid_android_target/LEToUpsideDownCake/valid_target.sh
@@ -1,6 +1,6 @@
 # shellcheck disable=SC2148
 valid_current_android_target_pass() {
-    if [[ "$1" -ge 32 && "$1" -le 35 ]]; then
+    if [[ "$1" -ge 33 && "$1" -le 35 ]]; then
         ui_print "*********************************************"
         ui_print "- 模块暂不支持Android 15，请选择正确版本的模块QwQ！！！"
         abort "*********************************************"
