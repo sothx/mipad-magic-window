58454df5a7e0451ef76b0413aeb20bce3a7e64ef
Sothx <i@sothx.com>
2024-09-23 20:38:12 +0800

    Update verify_functions.sh

--- module_src/verify_functions_extends/general-vanillaIceCream/verify_functions.sh
+++ module_src/verify_functions_extends/general-vanillaIceCream/verify_functions.sh
@@ -1,6 +1,6 @@
 # shellcheck disable=SC2148
 verify_android_api_has_pass() {
-    if [[ "$1" -ge 35 ]]; then
+    if [[ "$1" -ge 34 ]]; then
         ui_print "*********************************************"
         ui_print "- 模块仅支持Android 15+，请重新选择正确版本的模块QwQ！！！"
         abort "*********************************************"
