18de41518ca32cfb6cb81e5d98521ceb07d62d8f
sothx <i@sothx.com>
2024-09-25 18:44:37 +0800

    feat: update

--- module_src/verify_functions_extends/pipa-device-code/verify_functions.sh
+++ module_src/verify_functions_extends/pipa-device-code/verify_functions.sh
@@ -6,7 +6,7 @@ verify_android_api_has_pass() {
         ui_print "- 模块当前仅支持Android14且暂未适配Android 15，请重新选择正确版本的模块QwQ！！！"
         ui_print "- Android 15 推荐安装[小米平板安卓15测试(Beta)版]模块，专版模块会在Hyper OS 2.0发布后才会支持哦~"
         ui_print "- 您可以选择强制安装Android版本不受支持的模块，但可能导致系统出现各种异常，是否继续？"
-        ui_print "  音量+ ：哼，我就要装(强制安装)"
+        ui_print "  音量+ ：哼，我偏要装(强制安装)"
         ui_print "  音量- ：否"
         ui_print "*********************************************"
         key_check
