97456de7a24d91d5b3192d77c7798221522f54ff
sothx <i@sothx.com>
2024-11-12 04:57:01 +0800

    fix: 修复错误的打包字数

--- tasks/buildApplicationCount.js
+++ tasks/buildApplicationCount.js
@@ -13,7 +13,7 @@ let packageStack = {}
  * 统计适配应用总数
  */
 module.exports = function buildApplicationCount(cb) {
-  return src(options.use_mode === 'activityEmbedding' ? ['temp/embedded_rules_list.xml','temp/fixed_orientation_list.xml','temp/autoui_list.xml'] : 'temp/magicWindowFeature_magic_window_application_list.xml') // 指定XML文件的路径
+  return src(options.use_mode === 'activityEmbedding' ? (options.is_projection ? ['temp/embedded_rules_list_projection.xml','temp/fixed_orientation_list_projection.xml','temp/generic_rules_list_projection.xml'] : ['temp/embedded_rules_list.xml','temp/fixed_orientation_list.xml','temp/autoui_list.xml']) : 'temp/magicWindowFeature_magic_window_application_list.xml') // 指定XML文件的路径
     .pipe(gulpXML({
       callback: function (result) {
         const doc = new DOMParser().parseFromString(result, 'text/xml')
