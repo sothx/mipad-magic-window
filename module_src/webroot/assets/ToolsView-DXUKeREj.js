8d8711182cd7dc5b36c75d3a2fee99654dfec72e
sothx <i@sothx.com>
2024-12-23 00:05:00 +0800

    feat: update

--- module_src/webroot/assets/ToolsView-DXUKeREj.js
+++
@@ -1,9 +0,0 @@
-import{d,h as c,av as m,aw as p,ax as u,ay as g,az as f,aA as y,i as o,j as b,Q as _,aB as x,aC as k,r as s,k as S,g as t,n,l as v,m as h,b as M,e as P,f as D,q as O,s as w,t as B,aD as A}from"./index-D2KgF-ID.js";const C={class:"setting"},T={class:"mt-3"},N={class:"px-3 sm:px-0"},V={class:"mt-3"},H=d({__name:"ToolsView",setup(R){const a=c();m(),p(),u(),g(),f(),y();const r=o(()=>({theme:a.isDarkMode?w:B}));b(["message","modal"],{configProviderProps:r}),_(),x(),k();const i=s([{label:"MiSans",key:"MiSans",type:"info"},{label:"HarmonyOS Sans",key:"HarmonyOS Sans",type:"error"},{label:"OPPO Sans",key:"OPPO Sans",type:"success"}]);return o(()=>A(i.value,"key")),s(""),s(!1),(z,e)=>{const l=P("n-alert");return D(),S("div",C,[t("div",T,[t("div",N,[e[0]||(e[0]=t("h3",{class:n("text-base font-semibold leading-7")},[t("span",{class:"animated-bg bg-clip-text font-semibold text-transparent",style:{"background-image":`linear-gradient(\r
-								101.22deg,\r
-								rgb(255, 182, 133) -18.32%,\r
-								rgb(255, 111, 29) 7.01%,\r
-								rgb(252, 181, 232) 41.59%,\r
-								rgb(135, 148, 255) 70.98%,\r
-								rgb(60, 112, 255) 91.35%,\r
-								rgb(60, 112, 255) 110.17%\r
-							)`}},"百宝箱")],-1)),t("p",{class:n(`mt-1 max-w-2xl text-sm leading-6 ${v(a).isDarkMode?"text-gray-300":"text-gray-500"}`)}," 百宝箱，汇集了丰富的实用工具 ",2)]),t("div",V,[h(l,{title:"极暗模式",class:"cursor-pointer","show-icon":!1,type:"info"},{default:M(()=>e[1]||(e[1]=[O("点击可以快速打开支付宝的扫码界面")])),_:1})])])])}}});export{H as default};
