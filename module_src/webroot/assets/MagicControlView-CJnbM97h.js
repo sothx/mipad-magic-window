28cc3a3710e2640c67bbb8f9976330afbe0c91dc
sothx <i@sothx.com>
2025-02-17 03:42:02 +0800

    feat: update

--- module_src/webroot/assets/MagicControlView-CJnbM97h.js
+++
@@ -1,9 +0,0 @@
-import{d as p,h as f,aA as g,aB as u,aC as y,aD as _,U as b,aE as k,i as o,j as x,V as h,aF as S,aG as v,r as s,k as M,g as t,n,l as w,m as r,b as P,e as i,f as D,s as O,y as B,z as C,aH as A}from"./index-CnTJaur6.js";const V={class:"setting"},z={class:"mt-3"},N={class:"px-3 sm:px-0"},T={class:"mt-3"},F=p({__name:"MagicControlView",setup(E){const a=f();g(),u(),y(),_(),b(),k();const l=o(()=>({theme:a.isDarkMode?B:C}));x(["message","modal"],{configProviderProps:l}),h(),S(),v();const c=s([{label:"MiSans",key:"MiSans",type:"info"},{label:"HarmonyOS Sans",key:"HarmonyOS Sans",type:"error"},{label:"OPPO Sans",key:"OPPO Sans",type:"success"}]);return o(()=>A(c.value,"key")),s(""),s(!1),(H,e)=>{const d=i("n-watermark"),m=i("n-alert");return D(),M("div",V,[t("div",z,[t("div",N,[e[0]||(e[0]=t("h3",{class:n("text-base font-semibold leading-7")},[t("span",{class:"animated-bg bg-clip-text font-semibold text-transparent",style:{"background-image":`linear-gradient(\r
-								101.22deg,\r
-								rgb(255, 182, 133) -18.32%,\r
-								rgb(255, 111, 29) 7.01%,\r
-								rgb(252, 181, 232) 41.59%,\r
-								rgb(135, 148, 255) 70.98%,\r
-								rgb(60, 112, 255) 91.35%,\r
-								rgb(60, 112, 255) 110.17%\r
-							)`}},"外设按键映射")],-1)),t("p",{class:n(`mt-1 max-w-2xl text-sm leading-6 ${w(a).isDarkMode?"text-gray-300":"text-gray-500"}`)}," 管理有关外设按键映射的配置 ",2)]),r(d,{content:"开发中，尚未上线",cross:"",fullscreen:"","font-size":16,"line-height":16,width:384,height:384,"x-offset":12,"z-index":999,"y-offset":60,rotate:-15}),t("div",T,[r(m,{class:"cursor-pointer","show-icon":!1,type:"info"},{default:P(()=>e[1]||(e[1]=[O("该功能开发中，不会这么快上线，请勿催更，请期待后续更新。")])),_:1})])])])}}});export{F as default};
