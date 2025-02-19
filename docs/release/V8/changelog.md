ff70b00e8c56f8c361800bd0afc6098bf6cce92e
sothx <i@sothx.com>
2025-02-19 17:04:18 +0800

    feat: update

--- docs/release/V8/changelog.md
+++ docs/release/V8/changelog.md
@@ -6,11 +6,9 @@
 
 链接: [https://caiyun.139.com/m/i?135CeBMHACC6p](https://caiyun.139.com/m/i?135CeBMHACC6p)
 
-- 新增部分高频应用的定制模式白名单适配
+- 针对Android15版本的模块新增[模块设置-模块使用须知]，可以关闭每次安装模块的使用须知提醒。
 
-- 新增[精选应用-小睡眠]，模块已为小睡眠适配大屏
-
-- 新增心潮(com.heartide.xinchao.stressandroid)的强制横屏适配规则
+- 优化酷我在Android 15 Hyper OS 2的适配规则，避免竖屏转横屏
 
 如果是处于测试版(Beta)的模块，可能存在较多的不稳定性以及异常BUG，如遇问题可反馈，反馈Q群：277757185
 模块首页:[https://hyper-magic-window.sothx.com/](https://hyper-magic-window.sothx.com/)
--- module_src/verify_functions_extends/general-vanillaIceCream/verify_functions.sh
+++ module_src/verify_functions_extends/general-vanillaIceCream/verify_functions.sh
@@ -24,17 +24,19 @@ verify_android_api_has_pass() {
 
 verify_special_rule_pass() {
     local mi_os_version_code=$(grep_prop ro.mi.os.version.code /mi_ext/etc/build.prop)
+    local sothx_disabled_os2_install_module_tips=$(getprop ro.sothx.disabled_os2_install_module_tips)
     # 目录不存在则创建目录
     if [[ ! -d "$MODPATH/system/system_ext/framework" ]]; then
         /bin/mkdir -p "$MODPATH/system/system_ext/framework"
     fi
-    if [[ -z "$mi_os_version_code" || "$mi_os_version_code" == "2" || "$mi_os_version_code" -eq 2 ]]; then
+    if [[ -z "$sothx_disabled_os2_install_module_tips" ]]; then
         ui_print "*********************************************"
         ui_print "- 感谢使用<完美横屏应用计划>在Hyper OS 2.0版本~"
         ui_print "- 请了解以下使用须知："
         ui_print "- 1.Hyper OS 2.0的模块必须搭配Web UI使用~"
         ui_print "- 2.在[平板专区-应用横屏布局]所做的任何修改会在重启后丢失~"
         ui_print "- 3.如需修改应用横屏适配，请前往Web UI进行修改~"
+        ui_print "- 4.可前往[模块设置-模块使用须知]，禁用该提醒"
         ui_print "- (Tips:请随意选择，不影响模块安装过程~)"
         ui_print "  音量+ ：已了解使用须知"
         ui_print "  音量- ：已了解使用须知"
