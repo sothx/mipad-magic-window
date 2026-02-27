51ef1cd2c3053a5277e2d33dac2728de770b25b5
sothx <i@sothx.com>
2026-02-28 02:15:18 +0800

    feat: 完善应用名称获取

---
+++ module_src/webroot/assets/PlusIcon-_zGXuryX.js
@@ -0,0 +1,1 @@
+import{bq as r,br as o,bz as t}from"./apis-Dc_OHZ7a.js";function n(n,s){return o(),r("svg",{xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 24 24","stroke-width":"1.5",stroke:"currentColor","aria-hidden":"true","data-slot":"icon"},[t("path",{"stroke-linecap":"round","stroke-linejoin":"round",d:"M12 4.5v15m7.5-7.5h-15"})])}export{n as r};
