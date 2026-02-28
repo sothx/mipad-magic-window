8912ac3200bb4481035d1e90fbf3f6d1e9acf99d
sothx <i@sothx.com>
2026-02-28 21:04:34 +0800

    feat: update ver

---
+++ module_src/webroot/assets/PlusIcon-Djq0kevt.js
@@ -0,0 +1,1 @@
+import{bq as r,br as o,bz as t}from"./apis-C7ty3117.js";function n(n,s){return o(),r("svg",{xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 24 24","stroke-width":"1.5",stroke:"currentColor","aria-hidden":"true","data-slot":"icon"},[t("path",{"stroke-linecap":"round","stroke-linejoin":"round",d:"M12 4.5v15m7.5-7.5h-15"})])}export{n as r};
