ccf577eb9391a00dd66db1c5c196789153a9e37e
sothx <i@sothx.com>
2026-04-06 20:50:56 +0800

    feat: 优化启动逻辑

--- install_module_template/generic/common/source/auto_enable_mi_screen_shots_write_clipboard/auto_enable_mi_screen_shots_write_clipboard.sh
+++ install_module_template/generic/common/source/auto_enable_mi_screen_shots_write_clipboard/auto_enable_mi_screen_shots_write_clipboard.sh
@@ -1,3 +1,4 @@
 # shellcheck disable=SC2148
-# 截图自动添加至剪贴板
-settings put secure mi_screen_shots_write_clipboard_enable 1
+if [ "$(settings get secure mi_screen_shots_write_clipboard_enable)" !== "1" ]; then
+  settings put secure mi_screen_shots_write_clipboard_enable 1
+fi
