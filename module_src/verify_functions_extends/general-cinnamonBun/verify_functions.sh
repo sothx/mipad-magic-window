a804dede36d95b1301a5b2e7fa2f4631929dadc4
sothx <i@sothx.com>
2026-08-16 01:02:15 +0800

    feat: 修复Android 17 应用布局优化

--- module_src/verify_functions_extends/general-cinnamonBun/verify_functions.sh
+++ module_src/verify_functions_extends/general-cinnamonBun/verify_functions.sh
@@ -25,6 +25,40 @@ verify_android_api_has_pass() {
 verify_special_rule_pass() {
   local mi_os_version_code=$(grep_prop ro.mi.os.version.code /mi_ext/etc/build.prop)
   local sothx_disabled_os2_install_module_tips=$(getprop ro.sothx.disabled_os2_install_module_tips)
+  local is_need_install_autoui_cloudfix_apk=1
+  HAS_BEEN_INSTALLED_AutoUI_CloudFix_APK=$(pm list packages | grep io.github.sothx.autouicloudfix)
+  if [[ $HAS_BEEN_INSTALLED_AutoUI_CloudFix_APK == *"package:io.github.sothx.autouicloudfix"* ]]; then
+    is_need_install_autoui_cloudfix_apk=0
+  fi
+  if [[ $is_need_install_autoui_cloudfix_apk == 1 ]]; then
+    ui_print "*********************************************"
+    ui_print "- 是否安装AutoUI Cloud Fix？"
+    ui_print "- [重要提醒]: Hyper OS 4 需要安装该LSPosed模块才能修复应用布局优化不生效的问题"
+    ui_print "- [重要提醒]: 安装后请前往LSPosed启用模块并重启平板"
+    ui_print "  音量+ ：是"
+    ui_print "  音量- ：否"
+    ui_print "*********************************************"
+    key_check
+    if [[ "$keycheck" == "KEY_VOLUMEUP" ]]; then
+      ui_print "- 正在为你安装AutoUI Cloud Fix，请稍等~"
+      unzip -jo "$ZIPFILE" 'common/apks/AutoUICloudFix.apk' -d /data/local/tmp/ &>/dev/null
+      pm install -r /data/local/tmp/AutoUICloudFix.apk &>/dev/null
+      rm -rf /data/local/tmp/AutoUICloudFix.apk
+      HAS_BEEN_INSTALLED_AutoUI_CloudFix_APK=$(pm list packages | grep io.github.sothx.autouicloudfix)
+      if [[ $HAS_BEEN_INSTALLED_AutoUI_CloudFix_APK == *"package:io.github.sothx.autouicloudfix"* ]]; then
+        ui_print "- 好诶，AutoUI Cloud Fix安装完成！"
+        ui_print "- 好诶，安装后请前往LSPosed启用模块并重启平板！"
+      else
+        abort "- AutoUI Cloud Fix安装失败，请尝试重新安装！"
+        abort "- 也可前往模块网盘下载单独的 AutoUI Cloud Fix apk 进行手动安装！"
+      fi
+    else
+      ui_print "*********************************************"
+      ui_print "- 你选择不安装AutoUI Cloud Fix"
+      ui_print "- Hyper OS 4 需要安装该LSPosed模块才能修复应用布局优化不生效的问题"
+      ui_print "*********************************************"
+    fi
+  fi
   if [[ -z "$sothx_disabled_os2_install_module_tips" ]]; then
     ui_print "*********************************************"
     ui_print "- 感谢使用<完美横屏应用计划>Hyper OS 4.0版本~"
