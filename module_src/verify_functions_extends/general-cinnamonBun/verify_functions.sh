e9ab67a3a5833b94f2431943c59be4f332a3774c
sothx <i@sothx.com>
2026-08-19 03:40:45 +0800

    feat:兼容移植包

--- module_src/verify_functions_extends/general-cinnamonBun/verify_functions.sh
+++ module_src/verify_functions_extends/general-cinnamonBun/verify_functions.sh
@@ -25,6 +25,7 @@ verify_android_api_has_pass() {
 verify_special_rule_pass() {
   local mi_os_version_code=$(grep_prop ro.mi.os.version.code /mi_ext/etc/build.prop)
   local sothx_project_treble_support_autoui_services_fix=$(getprop ro.config.sothx_project_treble_support_autoui_services_fix)
+  local sothx_project_treble_autoui_services_fix_version=$(getprop ro.config.sothx_project_treble_autoui_services_fix_version)
   local sothx_disabled_os2_install_module_tips=$(getprop ro.sothx.disabled_os2_install_module_tips)
   local is_need_install_autoui_cloudfix_apk=1
   HAS_BEEN_INSTALLED_AutoUI_CloudFix_APK=$(pm list packages | grep io.github.sothx.autouicloudfix)
@@ -32,8 +33,11 @@ verify_special_rule_pass() {
     is_need_install_autoui_cloudfix_apk=0
   fi
   if [[ $sothx_project_treble_support_autoui_services_fix == 'true' ]]; then
-    is_need_install_autoui_cloudfix_apk=0
+    if [[ $sothx_project_treble_autoui_services_fix_version -ge 2 ]]; then
+      is_need_install_autoui_cloudfix_apk=0
+    fi
   fi
+
   if [[ $is_need_install_autoui_cloudfix_apk == 1 ]]; then
     ui_print "*********************************************"
     ui_print "- 是否安装AutoUI Cloud Fix？"
